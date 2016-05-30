d89ae7449f08c713167d64c02be19b2e9fae0ea5
kpuwal <kpuwal@gmail.com>
2016-05-30 13:52:23 +0100

    implemented limit feature

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,17 @@
 class Oystercard
-
+  MAXIMUM_BALANCE = 90
   attr_reader :balance
+  
   def initialize
+    @limit= MAXIMUM_BALANCE
     @balance= 0
   end
 
   def top_up(value)
+    fail "top up exceeds balance limit, cannot top up" if balance+value > limit
     @balance += value
   end
+
+  private
+  attr_reader :limit
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,14 +9,22 @@ describe Oystercard do
 
   describe '#top_up' do
     it{is_expected.to respond_to(:top_up).with(1).argument}
+
     it "increases balanced by expected value" do
       subject.top_up(5)
       expect(subject.balance).to eq 5
       subject.top_up(5)
       expect(subject.balance).to eq 10
     end
+
     it 'can top up the balance' do
       expect{subject.top_up 1}.to change{subject.balance}.by 1
+    end
+
+    it 'raises an error if top_up exceeds balance limit' do
+      expect { subject.top_up(100) }.to raise_error "top up exceeds balance limit, cannot top up"
+      subject.top_up(50)
+      expect { subject.top_up(50) }.to raise_error "top up exceeds balance limit, cannot top up"
     end
   end
 end
