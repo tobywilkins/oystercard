d3be7ad3c6acd16c71845f51d40107afc6ee6828
mtaner <mervetaner88@gmail.com>
2016-05-30 17:38:08 +0100

    Created touch_in/touch_out/in_journey? methods that stores use of card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,6 +6,8 @@ attr_reader :balance
 
   def initialize
     @balance = 0
+    @touched_in = false
+    @touched_out = false
   end
 
   def top_up(amount)
@@ -13,4 +15,20 @@ attr_reader :balance
   	@balance += amount
   end
 
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def touch_in
+    @touched_in = true
+  end
+
+  def touch_out
+    @touched_in = false
+    @touched_out = true
+  end
+
+  def in_journey?
+    @touched_in && @touched_out == false ? true : false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,11 +12,50 @@ describe Oystercard do
   		expect{subject.top_up(10)}.to change{subject.balance}.by 10
   	end
 
-  	it "should raise an error if balance reaches card capacity" do
+  	it "should raise an error if balance reaches card capacity." do
   		subject.top_up(Oystercard::MAXIMUM_BALANCE)
   		expect{subject.top_up(1)}.to raise_error("Maximum card balance exceeded. Maximum balance is: #{Oystercard::MAXIMUM_BALANCE}")
   	end
+  end
+
+  describe "#deduct" do
+
+    it "should decrease the balance with a specified fare." do
+      subject.top_up(10)
+      expect{subject.deduct(10)}.to change{subject.balance}.by -10
+    end
+  end
+
+  describe "#touch_in" do
+
+    it "should return true if user touched in" do
+      subject.top_up(5)
+      expect(subject.touch_in).to eq true
+    end
 
   end
+
+  describe "#touch_out" do
+
+    it "should return true if user touched out." do
+      subject.top_up(5)
+      expect(subject.touch_out).to eq true
+    end
+  end
+
+  describe "#in_journey?" do
+
+    it "should let oystercard know it is in a journey." do
+      subject.touch_in
+      expect(subject.in_journey?).to eq true
+    end
+
+    it "should not be in a journey if touched in and touched out." do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
+
 
 end
