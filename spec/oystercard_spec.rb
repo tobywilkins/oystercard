aa9afd8e224c4d518f04259299e4c84d6bba68ec
Luke <lukeaveil@yahoo.com>
2016-05-30 16:28:06 +0100

    added top up method to oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,15 @@
 require 'oystercard'
 
 describe Oystercard do
+
   it "should start with a balance of zero." do
-    expect(subject.balance). to eq 0
+    expect(subject.balance).to eq 0
+  end
+
+  describe "#top_up" do
+  	it "should increase the balance with a specified amount." do
+  		expect{subject.top_up(10)}.to change{subject.balance}.by 10
+  	end
   end
 
 end
